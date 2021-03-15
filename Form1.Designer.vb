3eefb5d85996e0f9ad384d0a2851647c30a470f4
nastya <nastya.vergeeva@mail.ru>
2021-03-15 12:12:36 +0500

    Добавьте файлы проекта.

---
+++ Form1.Designer.vb
@@ -0,0 +1,206 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Форма переопределяет dispose для очистки списка компонентов.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Является обязательной для конструктора форм Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'Примечание: следующая процедура является обязательной для конструктора форм Windows Forms
+    'Для ее изменения используйте конструктор форм Windows Form.  
+    'Не изменяйте ее в редакторе исходного кода.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.ListBox2 = New System.Windows.Forms.ListBox()
+        Me.ListBox3 = New System.Windows.Forms.ListBox()
+        Me.ListBox4 = New System.Windows.Forms.ListBox()
+        Me.ListBox5 = New System.Windows.Forms.ListBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(11, 16)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(143, 19)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Физическим лицам"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 205)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(157, 19)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Юридическим лицам"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(322, 16)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(269, 19)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Индивидуальным предпринимателям"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(322, 205)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(146, 19)
+        Me.Label4.TabIndex = 6
+        Me.Label4.Text = "Налоговые вычеты"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.AllowDrop = True
+        Me.ListBox1.Font = New System.Drawing.Font("Times New Roman", 10.8!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 20
+        Me.ListBox1.Items.AddRange(New Object() {"На доход физического лица", "Налог на прибыль", "Налог на доходы работников", "Вычет на обучение", "Взнос в пенсионный фонд", "Транспортный налог", "НДС", "Вычет на жилье", "НДС в цене любого товара", "Налог на имущество юридических лиц", "Уплата стоимости патента", "Вычет на лечение", "Взнос в фонд любого товара", "Земельный налог", "Налог на временный доход", "Вычет по ипотеке"})
+        Me.ListBox1.Location = New System.Drawing.Point(649, 39)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(325, 344)
+        Me.ListBox1.TabIndex = 8
+        '
+        'ListBox2
+        '
+        Me.ListBox2.AllowDrop = True
+        Me.ListBox2.FormattingEnabled = True
+        Me.ListBox2.ItemHeight = 19
+        Me.ListBox2.Location = New System.Drawing.Point(15, 38)
+        Me.ListBox2.Name = "ListBox2"
+        Me.ListBox2.Size = New System.Drawing.Size(294, 156)
+        Me.ListBox2.TabIndex = 9
+        '
+        'ListBox3
+        '
+        Me.ListBox3.AllowDrop = True
+        Me.ListBox3.FormattingEnabled = True
+        Me.ListBox3.ItemHeight = 19
+        Me.ListBox3.Location = New System.Drawing.Point(15, 227)
+        Me.ListBox3.Name = "ListBox3"
+        Me.ListBox3.Size = New System.Drawing.Size(294, 156)
+        Me.ListBox3.TabIndex = 10
+        '
+        'ListBox4
+        '
+        Me.ListBox4.AllowDrop = True
+        Me.ListBox4.FormattingEnabled = True
+        Me.ListBox4.ItemHeight = 19
+        Me.ListBox4.Location = New System.Drawing.Point(326, 39)
+        Me.ListBox4.Name = "ListBox4"
+        Me.ListBox4.Size = New System.Drawing.Size(294, 156)
+        Me.ListBox4.TabIndex = 11
+        '
+        'ListBox5
+        '
+        Me.ListBox5.AllowDrop = True
+        Me.ListBox5.FormattingEnabled = True
+        Me.ListBox5.ItemHeight = 19
+        Me.ListBox5.Location = New System.Drawing.Point(326, 227)
+        Me.ListBox5.Name = "ListBox5"
+        Me.ListBox5.Size = New System.Drawing.Size(294, 156)
+        Me.ListBox5.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(645, 16)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(126, 19)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = "Список налогов:"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(649, 389)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(325, 47)
+        Me.Button1.TabIndex = 14
+        Me.Button1.Text = "Проверить"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(15, 389)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(294, 47)
+        Me.Button2.TabIndex = 15
+        Me.Button2.Text = "Подсказка по использованию"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(326, 388)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(294, 47)
+        Me.Button3.TabIndex = 16
+        Me.Button3.Text = "Подсказка по заданию"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AllowDrop = True
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 19.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.ControlLight
+        Me.ClientSize = New System.Drawing.Size(1002, 447)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.ListBox5)
+        Me.Controls.Add(Me.ListBox4)
+        Me.Controls.Add(Me.ListBox3)
+        Me.Controls.Add(Me.ListBox2)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Font = New System.Drawing.Font("Times New Roman", 10.2!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(204, Byte))
+        Me.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.Name = "Form1"
+        Me.Text = "Налоги"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents ListBox2 As ListBox
+    Friend WithEvents ListBox3 As ListBox
+    Friend WithEvents ListBox4 As ListBox
+    Friend WithEvents ListBox5 As ListBox
+    Friend WithEvents Label5 As Label
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,146 @@
+﻿Public Class Form1
+
+    Function FindIncorrect(LB As ListBox, CorrectAnswers As List(Of String)) As List(Of String)
+        Dim Incorrect As List(Of String) = New List(Of String) 'Создаем массив для ошибочных вариантов
+        For Each item As String In LB.Items 'Для каждого элемента в ЛистБоксе
+            Dim flag As Boolean = True 'Флаг для проверки, найден ли элемент в списке верных ответов
+            For index = 0 To CorrectAnswers.Count - 1 'Идем по массиву верных ответов
+                If item = CorrectAnswers.Item(index) Then 'Если ответ правильный
+                    flag = False
+                    Exit For
+                End If
+            Next
+            If flag Then 'Если элемент неверный
+                Incorrect.Add(item) 'Добавляем в список неверных ответов
+            End If
+        Next
+        Return Incorrect  'Возвращаем массив неверных ответов
+    End Function
+    Private Sub ListBox5_MouseDown(sender As Object, e As MouseEventArgs) Handles ListBox5.MouseDown, ListBox4.MouseDown, ListBox3.MouseDown, ListBox2.MouseDown, ListBox1.MouseDown
+        Dim src As ListBox = DirectCast(sender, ListBox)
+        If e.Button = Windows.Forms.MouseButtons.Left Then
+            For i As Integer = 0 To src.Items.Count - 1
+                If src.GetItemRectangle(i).Contains(e.Location) Then
+                    src.DoDragDrop(src, DragDropEffects.Move)
+                    Exit For
+                End If
+            Next
+        End If
+
+    End Sub
+
+    Private Sub ListBox5_DragEnter(sender As Object, e As DragEventArgs) Handles ListBox5.DragEnter, ListBox4.DragEnter, ListBox3.DragEnter, ListBox2.DragEnter, ListBox1.DragEnter
+        Dim src As ListBox = DirectCast(sender, ListBox)
+        If e.Data.GetDataPresent("System.Windows.Forms.ListBox") Then
+            e.Effect = DragDropEffects.Move
+        End If
+    End Sub
+
+    Private Sub ListBox5_DragDrop(sender As Object, e As DragEventArgs) Handles ListBox5.DragDrop, ListBox4.DragDrop, ListBox3.DragDrop, ListBox2.DragDrop, ListBox1.DragDrop
+        Dim src As ListBox = DirectCast(sender, ListBox)
+        If e.Data.GetDataPresent("System.Windows.Forms.ListBox") Then
+            Dim data As ListBox = DirectCast(e.Data.GetData("System.Windows.Forms.ListBox"), ListBox)
+            Dim item As Object = data.SelectedItem
+            data.Items.Remove(item)
+            Dim location As Point = src.PointToClient(New Point(e.X, e.Y))
+            Dim dropIndex As Integer = -1
+            For i As Integer = 0 To src.Items.Count - 1
+                If src.GetItemRectangle(i).Contains(location) Then
+                    dropIndex = i
+                    Exit For
+                End If
+            Next
+            If dropIndex = -1 Then
+                src.Items.Add(item)
+            Else
+                src.Items.Insert(dropIndex, item)
+            End If
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        MessageBox.Show("Чтобы распределить налоги по категориям, перетащите их из списка в окна", "Подсказка по использованию")
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        MessageBox.Show("1. В каждом окне по 4 налога
+2. Читайте внимательно ключевые слова в названии налогов", "Подсказка по заданию")
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim CorrectAnswers As List(Of String) = New List(Of String) 'Массив корректных ответов
+        Dim Result As List(Of String) 'Массив для некорректных ответов
+        Dim CorrectCounter As Integer = 0 'счетчик для корректности всех ЛистБоксов
+        'Физическим лицам
+        CorrectAnswers.Add("На доход физического лица") 'Добавление правильных ответов в массив
+        CorrectAnswers.Add("Взнос в пенсионный фонд")
+        CorrectAnswers.Add("НДС в цене любого товара")
+        CorrectAnswers.Add("Взнос в фонд любого товара")
+        Result = FindIncorrect(ListBox2, CorrectAnswers) 'Используем функцию для проверки
+        If Result.Count <> 0 Then 'если массив с неверными ответами не пустой
+            For index = 0 To Result.Count - 1
+                ListBox2.Items.Remove(Result.Item(index)) 'Удаляем из текущего списка
+                ListBox1.Items.Add(Result.Item(index)) 'Добавляем в изначальный
+            Next
+        Else CorrectCounter += 1
+        End If  'Для остальных списков алгоритм идентичный
+
+        'Юридическим лицам
+        CorrectAnswers.Clear()
+        CorrectAnswers.Add("Налог на прибыль")
+        CorrectAnswers.Add("Земельный налог")
+        CorrectAnswers.Add("Налог на имущество юридических лиц")
+        CorrectAnswers.Add("Транспортный налог")
+        Result = FindIncorrect(ListBox3, CorrectAnswers)
+        If Result.Count <> 0 Then
+            For index = 0 To Result.Count - 1
+                ListBox3.Items.Remove(Result.Item(index))
+                ListBox1.Items.Add(Result.Item(index))
+            Next
+        Else CorrectCounter += 1
+        End If
+        'Индивидуальным предпринимателям
+        CorrectAnswers.Clear()
+        CorrectAnswers.Add("Налог на доходы работников")
+        CorrectAnswers.Add("НДС")
+        CorrectAnswers.Add("Уплата стоимости патента")
+        CorrectAnswers.Add("Налог на временный доход")
+        Result = FindIncorrect(ListBox4, CorrectAnswers)
+        If Result.Count <> 0 Then
+            For index = 0 To Result.Count - 1
+                ListBox4.Items.Remove(Result.Item(index))
+                ListBox1.Items.Add(Result.Item(index))
+            Next
+        Else CorrectCounter += 1
+        End If
+        'Налоговые вычеты
+        CorrectAnswers.Clear()
+        CorrectAnswers.Add("Вычет на обучение")
+        CorrectAnswers.Add("Вычет на жилье")
+        CorrectAnswers.Add("Вычет на лечение")
+        CorrectAnswers.Add("Вычет по ипотеке")
+        Result = FindIncorrect(ListBox5, CorrectAnswers)
+        If Result.Count <> 0 Then
+            For index = 0 To Result.Count - 1
+                ListBox5.Items.Remove(Result.Item(index))
+                ListBox1.Items.Add(Result.Item(index))
+            Next
+        Else CorrectCounter += 1
+        End If
+
+        If CorrectCounter = 4 Then
+            If ListBox1.Items.Count = 0 Then
+                MessageBox.Show("Все верно!", "Поздравление")
+            Else MessageBox.Show("Вы движетесь в верном направлении, но распределите элементы до конца!", "Предупреждение")
+            End If
+        Else
+            MessageBox.Show("Неверно. Попробуйте снова :( 
+Неккоректные варианты переместились назад в список", "Предупреждение")
+        End If
+    End Sub
+End Class
